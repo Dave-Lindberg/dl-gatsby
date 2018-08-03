9d015b2fb2fc67fde30801c721871b183289f746
DaveLindberg <dave@davelindberg.com>
2018-08-02 22:52:31 -0400

    updated TagManager settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,11 +35,11 @@ module.exports = {
 
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+        includeInDevelopment: false
 
         // Specify optional GTM environment details.
-        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING',
-        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME'
+        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING',
+        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME'
       }
     },
     {
