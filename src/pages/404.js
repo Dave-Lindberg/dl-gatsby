bab0d6a02d9968d78d4ccde2e14c91c4f5aca7a5
DaveLindberg <dave@davelindberg.com>
2019-03-07 18:41:22 -0500

    fixed error on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,15 +4,16 @@ const NotFoundPage = () => (
   <div>
     <h1>Oops.</h1>
     <p>The page you're looking for is currently offline. </p>
+    {/* A little help for the Netlify bots if you\'re not using a SSG */}
+    <form name="contact" netlify netlify-honeypot="bot-field" action="/index.html" hidden>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <input type="text" name="phone" />
+      <textarea name="user_message"></textarea>
+    </form>
+
   </div>
 
-  {/* A little help for the Netlify bots if you\'re not using a SSG */}
-  <form name="contact" netlify netlify-honeypot="bot-field" action="/index.html" hidden>
-    <input type="text" name="name" />
-    <input type="email" name="email" />
-    <input type="text" name="phone" />
-    <textarea name="user_message"></textarea>
-  </form>
 
 );
 
