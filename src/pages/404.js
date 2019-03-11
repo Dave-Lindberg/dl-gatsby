9568e531581d772431c7499754fe437c9b846e9b
DaveLindberg <dave@davelindberg.com>
2019-03-11 11:10:32 -0400

    changed html contact form field names to match React field names

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ const NotFoundPage = () => (
     <p>The page you're looking for is currently offline. </p>
     {/* A little help for the Netlify bots if you\'re not using a SSG */}
     <form name="contact" netlify netlify-honeypot="bot-field" action="/index.html" hidden>
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <input type="text" name="phone" />
+      <input type="text" name="user_name" />
+      <input type="email" name="user_email" />
+      <input type="text" name="user_phone" />
       <textarea name="user_message"></textarea>
     </form>
 
